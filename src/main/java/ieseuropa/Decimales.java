6c49564a102b9bc852e683ab3be252479d637d02
NachoS <secaduras87@gmail.com>
2023-03-22 13:26:42 +0100

    Añadido método para eliminar los decimales del número.

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -1,9 +1,13 @@
 package main.java.ieseuropa;
 
 public class Decimales {
-	
+
 	private static int redondearDecimal(double num) {
 		return (int) Math.ceil(num);
+	}
+
+	private static int eliminarDecimal(double num) {
+		return (int) num;
 	}
 
 	public static void main(String[] args) {
@@ -11,6 +15,7 @@ public class Decimales {
 		double numeroDecimal = 4.145;
 		System.out.println("El número original es " + numeroDecimal);
 		System.out.println("El número redondeado es " + redondearDecimal(numeroDecimal));
+		System.out.println("El número eliminando decimales es " + eliminarDecimal(numeroDecimal));
 
 	}
 
