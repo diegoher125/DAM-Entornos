94058a8032b8f465f18b19a5640b638d8b7e8d23
NachoS <secaduras87@gmail.com>
2023-03-22 13:27:25 +0100

    Añadido método para saber ecuación de segundo grado.

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -10,12 +10,28 @@ public class Decimales {
 		return (int) num;
 	}
 
+	public static void ecuacionSegundoGrado(double a, double b, double c) {
+		double discriminante = Math.pow(b, 2) - 4 * a * c;
+		if (discriminante < 0) {
+			System.out.println("La ecuación no tiene soluciones reales.");
+		} else if (discriminante == 0) {
+			double x = -b / (2 * a);
+			System.out.println("La ecuación tiene una solución real: " + x);
+		} else {
+			double x1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+			double x2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+			System.out.println("La ecuación tiene dos soluciones reales: " + x1 + " y " + x2);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		double numeroDecimal = 4.145;
 		System.out.println("El número original es " + numeroDecimal);
 		System.out.println("El número redondeado es " + redondearDecimal(numeroDecimal));
 		System.out.println("El número eliminando decimales es " + eliminarDecimal(numeroDecimal));
+		System.out.println("Ecuación de segundo grado: ");
+		ecuacionSegundoGrado(1.0, -3.0, 2.0);
 
 	}
 
